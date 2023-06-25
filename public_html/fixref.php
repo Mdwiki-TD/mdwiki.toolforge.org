838e8650ac16a0be1e6257e4db0ccee36a048722
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-06-25 22:15:05 +0300

    Update fixref.php

--- public_html/fixref.php
+++ public_html/fixref.php
@@ -9,6 +9,7 @@ HTML;
 //---
 $titlelist  = $_REQUEST['titlelist'] ?? '';
 $number     = $_REQUEST['number'] ?? '';
+$test       = $_REQUEST['test'] ?? '';
 //---
 function writee($file,$text) {
 	$myfile = fopen( 'find/' . $file, "w");
@@ -18,40 +19,41 @@ function writee($file,$text) {
 //---
 function make_form() {
 	//---
-	global $titlelist, $number;
+	global $titlelist, $number, $test;
 	//---
-	$number2 = $number;
+	$testinput = ($test != '') ? '<input type="hidden" name="test" value="1" />' : '';
 	//---
 	echo <<<HTML
 	<form action='fixref.php' method='POST'>    
-	<div class='container'>
+		$testinput
 		<div class='container'>
-			<div class='row'>
-				<div class='col-lg-12'>
-				<div class='form-group'>
-					<label for='find'><h4>All pages:</h4></label>
-					<div class='input-group mb-3'>
-						<div class='input-group-prepend'>
-							<span class='input-group-text'>Number of pages</span>
+			<div class='container'>
+				<div class='row'>
+					<div class='col-lg-12'>
+						<div class='form-group'>
+							<label for='find'><h4>All pages:</h4></label>
+							<div class='input-group mb-3'>
+								<div class='input-group-prepend'>
+									<span class='input-group-text'>Number of pages</span>
+								</div>
+								<input class='form-control' type='number' name='number' value='$number' placeholder='5000'/>
+							</div>
 						</div>
-						<input class='form-control' type='number' name='number' value='$number' placeholder='5000'/>
 					</div>
-				</div>
-				</div>
-				<div class='col-lg-12'>
-				<h3 class='aligncenter'>or</h3>
-				</div>
-				<div class='col-lg-12'>
-				<div class='form-group'>
-					<label for='titlelist'><h4>List of titles:</h4></label>
-					<textarea class='form-control' cols='60' rows='7' id='titlelist' name='titlelist'>$titlelist</textarea>
-				</div>
-				</div>
-				<div class='col-lg-12'>
-					<h4 class='aligncenter'>
-						<input class='btn btn-primary' type='submit' value='send'>
-					</h4>
-				</div>
+					<div class='col-lg-12'>
+						<h3 class='aligncenter'>or</h3>
+					</div>
+					<div class='col-lg-12'>
+						<div class='form-group'>
+							<label for='titlelist'><h4>List of titles:</h4></label>
+							<textarea class='form-control' cols='60' rows='7' id='titlelist' name='titlelist'>$titlelist</textarea>
+						</div>
+					</div>
+					<div class='col-lg-12'>
+						<h4 class='aligncenter'>
+							<input class='btn btn-primary' type='submit' value='send'>
+						</h4>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -66,20 +68,32 @@ if ($number == '' && $titlelist == '') {
 if ($number != '' or $titlelist != '') {
 	//---
 	$nn = rand();
-	$jsub = 'jsub -N fixref' . '$nn $HOME/local/bin/python3 core8/pwb.py mdpy/fixref';
+	$jsub = "jsub -N fixref$nn " . ' $HOME/local/bin/python3 core8/pwb.py mdpy/fixref';
+	//---
+	// trim $titlelist
+	$titlelist = trim($titlelist);
+	//---
+	// split py lines
+	$lines = explode("\n", $titlelist);
+	// if lenth of lines == 1 then
 	//---
 	if ( $titlelist != '' ) {
-		$filename = $nn . '_fix_ref_list.txt';
-		writee( $filename , $titlelist );
-		$jsub .= " -file:" . $filename;
-		//---
+		if ( count($lines) == 1 ) {
+			$title= $lines[0];
+			$jsub .= " -title:$title";
+		} else {
+			$filename = $nn . '_fix_ref_list.txt';
+			writee( $filename , $titlelist );
+			$jsub .= " -file:$filename";
+			//---
+		}
 	} elseif ( $number != '' ) {
 		$jsub .= " allpages -number:" . $number;
 	};
 	//---
 	echo "<h4 style='color:green'>The bot will start in seconds.</h4>";
 	//---
-	if (isset($_REQUEST['test'])) print $jsub;
+	if ($test != '') print $jsub;
 	//---
 	$result = shell_exec($jsub);
 	print $result;
