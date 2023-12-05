9bdaaeba476ab351724ee08c38c6b371c950be99
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-12-05 22:03:11 +0300

    create submodule for python files

--- md_core/mdpages/findqids.sh
+++
@@ -1,5 +0,0 @@
-python3 core8/pwb.py mdpages/get_md_to_en
-python3 core8/pwb.py mdpages/find_qids redirects addthem
-python3 core8/pwb.py mdpages/fixqids
-python3 core8/pwb.py mdpages/get_red
-python3 core8/pwb.py mdpages/P11143 add addq
--- md_core/new_api/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# (C) Ibrahem Qasim 2023s
-#
-#
