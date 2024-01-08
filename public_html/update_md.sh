352741204de3d834868d1c8105b50153b6803817
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-01-09 01:28:26 +0300

    Update public_html/update_md.sh
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- public_html/update_md.sh
+++ public_html/update_md.sh
@@ -4,6 +4,6 @@ export PATH=/data/project/mdwiki/local/bin:/usr/local/bin:/usr/bin:/bin
 
 toolforge jobs run updatemd --command "/data/project/mdwiki/update_mdcore.sh" --image mariadb
 
-echo "toolforge jobs run updatemd --command "/data/project/mdwiki/pybot/update_mdcore.sh" --image mariadb"
+echo "toolforge jobs run updatemd --command "/data/project/mdwiki/update_mdcore.sh" --image mariadb"
 
 toolforge jobs list
