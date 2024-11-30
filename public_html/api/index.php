861ebdd19b30bebdc7673ac3f497b443135ac2f5
ibrahem <ibrahem.al-radaei@outlook.com>
2024-11-30 04:32:54 +0300

    Update index.php

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -5,10 +5,11 @@ if (isset($_REQUEST['test'])) {
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
+/*
 if (!isset($_GET['get'])) {
     header('Location: /api/test/index.php');
     // include_once __DIR__ . '/test/index.php';
     exit();
 }
-
+*/
 include_once __DIR__ . '/api_cod/request.php';
