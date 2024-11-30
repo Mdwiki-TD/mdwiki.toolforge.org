760752f713864b8e16fd4241a426e587e5b59471
ibrahem <ibrahem.al-radaei@outlook.com>
2024-11-30 04:57:00 +0300

    Update request.php

--- public_html/api/api_cod/request.php
+++ public_html/api/api_cod/request.php
@@ -106,16 +106,28 @@ function make_status_query()
 }
 
 $DISTINCT = (isset($_GET['distinct'])) ? 'DISTINCT ' : '';
-$SELECT   = (isset($_GET['select'])) ? filter_input(INPUT_GET, 'select', FILTER_SANITIZE_SPECIAL_CHARS) : '*';
 $get = filter_input(INPUT_GET, 'get', FILTER_SANITIZE_SPECIAL_CHARS); //$_GET['get']
 
-// if (!isset($_GET['limit'])) $_GET['limit'] = '50';
+$select_valids = [
+    'count(title) as count',
+    'YEAR(pupdate) AS year',
+    'lang',
+    'user',
+];
+
+$SELECT   = (isset($_GET['select'])) ? filter_input(INPUT_GET, 'select', FILTER_SANITIZE_SPECIAL_CHARS) : '*';
+
+if (!in_array($SELECT, $select_valids)) {
+    $SELECT = '*';
+};
 
 $qua = "";
 $query = "";
 $params = [];
 $results = [];
 $execution_time = 0;
+
+// if (!isset($_GET['limit'])) $_GET['limit'] = '50';
 
 switch ($get) {
     case 'users':
