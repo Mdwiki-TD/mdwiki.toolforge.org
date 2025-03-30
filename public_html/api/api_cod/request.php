3403944375c3f6f3c1cedc78ca67c289a07b5ea6
ibrahem <ibrahem.al-radaei@outlook.com>
2025-03-30 03:10:00 +0300

    1

--- public_html/api/api_cod/request.php
+++ public_html/api/api_cod/request.php
@@ -143,19 +143,6 @@ switch ($get) {
         $params = $status['params'];
         break;
 
-    case 'views_old':
-        $query = <<<SQL
-            SELECT *
-            FROM views v
-            LEFT JOIN pages p
-                ON p.target = v.target
-                AND p.lang = v.lang
-        SQL;
-        $tab = add_li_params($query, [], $endpoint_params);
-        $query = $tab['qua'];
-        $params = $tab['params'];
-        break;
-
     case 'views':
     case 'views_new':
         $query = <<<SQL
@@ -335,7 +322,8 @@ switch ($get) {
     case 'pages_by_user_or_lang':
         // ---
         $qua = <<<SQL
-            SELECT DISTINCT *
+            SELECT DISTINCT p.title, p.word, p.translate_type, p.cat, p.lang, p.user, p.target, p.date,
+            p.pupdate, p.add_date, p.deleted, v.views
             FROM pages p
             LEFT JOIN views_new_all v
                 ON p.target = v.target
