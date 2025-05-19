3469dd39f7521298c9d7e2ea1517064268da099b
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-05-20 01:15:41 +0300

    Add support for 'titles' parameter in API queries and update endpoint_params.json

--- public_html/api/api_cod/subs/titles_infos.php
+++ public_html/api/api_cod/subs/titles_infos.php
@@ -8,35 +8,52 @@ use function API\TitlesInfos\titles_query;
 
 use function API\Helps\add_li_params;
 
+$qua_old = <<<SQL
+    SELECT
+        ase.title,
+        ase.importance,
+        rc.r_lead_refs,
+        rc.r_all_refs,
+        ep.en_views,
+        w.w_lead_words,
+        w.w_all_words,
+        q.qid
+    FROM assessments ase
+    LEFT JOIN enwiki_pageviews ep ON ase.title = ep.title
+    LEFT JOIN qids q ON q.title = ase.title
+    LEFT JOIN refs_counts rc ON rc.r_title = ase.title
+    LEFT JOIN words w ON w.w_title = ase.title
+SQL;
+
 function titles_query($endpoint_params)
 {
-    // ---
-    $qua_old = <<<SQL
-        SELECT
-            ase.title,
-            ase.importance,
-            rc.r_lead_refs,
-            rc.r_all_refs,
-            ep.en_views,
-            w.w_lead_words,
-            w.w_all_words,
-            q.qid
-        FROM assessments ase
-        LEFT JOIN enwiki_pageviews ep ON ase.title = ep.title
-        LEFT JOIN qids q ON q.title = ase.title
-        LEFT JOIN refs_counts rc ON rc.r_title = ase.title
-        LEFT JOIN words w ON w.w_title = ase.title
-    SQL;
     // ---
     $qua = <<<SQL
         SELECT *
         FROM titles_infos
     SQL;
     // ---
+    // remove titles from $endpoint_params { "name": "titles", "column": "titles", "type": "array" }
+    $endpoint_params = array_filter($endpoint_params, function ($param) {
+        return $param['name'] !== 'titles';
+    });
+    // ---
     $tab = add_li_params($qua, [], $endpoint_params);
     // ---
     $qua = $tab['qua'];
     $params = $tab['params'];
+    // ---
+    $where_or_and = (strpos(strtolower($qua), 'where') !== false) ? ' AND ' : ' where ';
+    // ---
+    // var_export($_GET);
+    // ---
+    $titles = $_GET['titles'] ?? [];
+    // ---
+    if (!empty($titles) && is_array($titles)) {
+        $placeholders = rtrim(str_repeat('?,', count($titles)), ',');
+        $qua .= " $where_or_and title IN ($placeholders)";
+        $params = array_merge($params, $titles);
+    }
     // ---
     return ["qua" => $qua, "params" => $params];
 }
