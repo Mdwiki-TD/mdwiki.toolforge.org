80ae95ac76ba9c0c9fc77ef0b4f1208189c513a0
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-05-29 04:15:37 +0300

    Update api.php

--- public_html/api.php
+++ public_html/api.php
@@ -1,8 +1,20 @@
 <?php
+if (isset($_REQUEST['test'])) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+};
 
 if (!isset($_GET['get'])) {
     header("Location: api/test.html");
     exit();
 }
 
-require(__DIR__ . '/api/index.php');
+$path = __DIR__ . '/api/index.php';
+
+if (!file_exists($path)) {
+    // I:\mdwiki\TD_API
+    $path = __DIR__ . '/../../TD_API/index.php';
+}
+
+require($path);
