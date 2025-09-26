961a2a31ccc4387c8c6b0824a9479029bb888175
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-09-27 02:15:52 +0300

    Update copy_to_medwiki.sh

--- jobs/copy_to_medwiki.sh
+++ jobs/copy_to_medwiki.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 $HOME/local/bin/python3 core8/pwb.py copy_to_en/revid
-$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki
-$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki slash
-$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki slash nodone
-$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki nodone
+#$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki
+#$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki slash
+#$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki slash nodone
+#$HOME/local/bin/python3 core8/pwb.py copy_to_en/medwiki nodone
