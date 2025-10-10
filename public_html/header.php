02204f8f40a7c19f2b7be9b8596bd695efb5481d
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-10 03:38:13 +0300

    Update header.php

--- public_html/header.php
+++ public_html/header.php
@@ -13,6 +13,7 @@ echo <<<HTML
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="robots" content="noindex">
+	<link rel="icon" href="/favicon.svg" sizes="any">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
