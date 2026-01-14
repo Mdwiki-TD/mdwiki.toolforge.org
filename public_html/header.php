e0c5ad4ab6bac01d2daa6e02535e33dc377544ea
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-14 03:58:16 +0300

    Update header.php

--- public_html/header.php
+++ public_html/header.php
@@ -165,6 +165,11 @@ echo <<<HTML
 							</a>
 						</li>
 						<li class="nav-item col-4 col-lg-auto">
+							<a class="nav-link py-2 px-0 px-lg-2" href="/views">
+								<span class="navtitles">PageViews</span>
+							</a>
+						</li>
+						<li class="nav-item col-4 col-lg-auto">
 							<a class="nav-link py-2 px-0 px-lg-2" href="https://github.com/MrIbrahem/mdwiki.toolforge.org" target="_blank">
 								<span class="navtitles">Github</span>
 							</a>
