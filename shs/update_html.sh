b672529c857b4405da8b61487c6212d0b8da5cf2
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-01 06:23:22 +0300

    Enable compile/remove flags in shs scripts
    Set COMPILE_PYTHON_FILES=1 in update_editor_stats.sh and update_pybot.sh so Toolforge jobs will compile Python files during deployment. Add REMOVE_SRC_JSONS_BEFORE_COPY=1 to update_html.sh to remove source JSONs before copying to public_html. These environment flags adjust install/copy behavior for the deployment scripts.

--- shs/update_html.sh
+++ shs/update_html.sh
@@ -2,6 +2,8 @@
 # toolforge-jobs run updatehtml --image python3.11 --command "~/shs/update_html.sh" --wait
 export SUB_DIR_COPY="public_html"
 export CLEAN_INSTALL=0
+export REMOVE_SRC_JSONS_BEFORE_COPY=1
+
 export USER_NAME="Mdwiki-TD"
 BRANCH="${1:-main}"
 REPO_NAME="mdwiki.toolforge.org"
--- shs/update_pybot.sh
+++ shs/update_pybot.sh
@@ -2,6 +2,7 @@
 # toolforge-jobs run updatepybot --image python3.11 --command "~/shs/update_pybot.sh" --wait
 export SUB_DIR_COPY=""
 export CLEAN_INSTALL=0
+export COMPILE_PYTHON_FILES=1
 export USER_NAME="Mdwiki-TD"
 BRANCH="${1:-main}"
 REPO_NAME="mdwiki-python-files"
