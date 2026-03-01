2f049a85e4ebe3bbbdb8294e347b2d29bb9556f5
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-01 06:25:44 +0300

    Update update_tdc.sh

--- shs/update_tdc.sh
+++ shs/update_tdc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # toolforge-jobs run updatetdc --image python3.11 --command "~/shs/update_tdc.sh" --wait
 export SUB_DIR_COPY="src"
-export CLEAN_INSTALL=1
+export CLEAN_INSTALL=0
 export USER_NAME="Mdwiki-TD"
 BRANCH="${1:-main}"
 REPO_NAME="tdc"
