e343c67d721ce176f81ca7e3931f520c43dfcda3
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-01 03:27:15 +0300

    Update update_td.sh

--- shs/update_td.sh
+++ shs/update_td.sh
@@ -4,8 +4,10 @@
 export SUB_DIR_COPY="src"
 export CLEAN_INSTALL=0
 export USER_NAME="Mdwiki-TD"
+export BASE_DIR="$HOME"
+
 BRANCH="${1:-main}"
-REPO_NAME=Translation-Dashboard
+REPO_NAME="Translation-Dashboard"
 REPO_PATH="public_html/Translation_Dashboard"
 
 $HOME/shs/deploy_repo.sh "$REPO_NAME" "$REPO_PATH" "$BRANCH"
