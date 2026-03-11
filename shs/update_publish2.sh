02fa5d8f2bf4d9c247262183f29ad014e21ebd3e
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-11 03:58:03 +0300

    Create update_publish2.sh

---
+++ shs/update_publish2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# toolforge-jobs run updatepublish2 --image bookworm --command "~/shs/update_publish2.sh update" --wait
+export USER_NAME="Mdwiki-TD"
+
+export SUB_DIR_COPY="src"
+export CLEAN_INSTALL=0
+export REMOVE_SRC_JSONS_BEFORE_COPY=1
+
+BRANCH="${1:-main}"
+REPO_NAME="publish"
+REPO_PATH="public_html/publish"
+
+$HOME/shs/deploy_repo.sh "$REPO_NAME" "$REPO_PATH" "$BRANCH"
+
