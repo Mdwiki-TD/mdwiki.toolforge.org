3ac7c8422fc3564c5aa44467123e39691407fa0d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-11 04:04:42 +0300

    Update update_publish2.sh

--- shs/update_publish2.sh
+++ shs/update_publish2.sh
@@ -13,5 +13,13 @@ REPO_PATH="public_html/publish"
 # copy `~/public_html/publish/all_pages_revids.json` to `~/data` before start deploy, overwrite, make sure `~/data` exist
 ALL_PAGES_REVIDS_PATH="$HOME/public_html/publish/all_pages_revids.json"
 
+# Ensure ~/data directory exists
+mkdir -p "$HOME/data"
+
+# Copy all_pages_revids.json to ~/data (overwrite if exists)
+if [ -f "$ALL_PAGES_REVIDS_PATH" ]; then
+    cp "$ALL_PAGES_REVIDS_PATH" "$HOME/data/"
+fi
+
 $HOME/shs/deploy_repo.sh "$REPO_NAME" "$REPO_PATH" "$BRANCH"
 
