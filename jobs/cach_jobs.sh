3901e5808405a561891bc92606cea4f3372f39b6
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-12 05:12:03 +0300

    .

--- jobs/cach_jobs.sh
+++ jobs/cach_jobs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Create temporary files with lists of existing articles for all languages
-$HOME/local/bin/python3 core8/pwb.py mdpages/cashwd
+$HOME/local/bin/python3 core8/pwb.py td_core/mdpages/cashwd
 
-$HOME/local/bin/python3 core8/pwb.py copy_data/by_qid/sitelinks
+$HOME/local/bin/python3 core8/pwb.py td_core/copy_data/by_qid/sitelinks
 
 # apis/cat_cach
-$HOME/local/bin/python3 core8/pwb.py apis/cat_cach
+$HOME/local/bin/python3 core8/pwb.py md_core_helps/apis/cat_cach
 
-$HOME/local/bin/python3 core8/pwb.py copy_data/by_title/all_articles
-$HOME/local/bin/python3 core8/pwb.py copy_data/by_title/exists_db
-
-#
-$HOME/local/bin/python3 core8/pwb.py wd_works/recheck
+$HOME/local/bin/python3 core8/pwb.py td_core/copy_data/by_title/all_articles
+$HOME/local/bin/python3 core8/pwb.py td_core/copy_data/by_title/exists_db
 
 #
-$HOME/local/bin/python3 core8/pwb.py db_work/check_titles
+$HOME/local/bin/python3 core8/pwb.py td_core/wd_works/recheck
+
+#
+$HOME/local/bin/python3 core8/pwb.py td_core/db_work/check_titles
