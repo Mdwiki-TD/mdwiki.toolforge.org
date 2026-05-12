96a6585ad95b166ee993814ab55cb09d55dea09b
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-12 06:05:32 +0300

    .

--- jobs/count_refs_words.sh
+++ jobs/count_refs_words.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 $HOME/local/bin/python3 core8/pwb.py td_core/mdcount/countref newpages
 $HOME/local/bin/python3 core8/pwb.py td_core/mdcount/countref sql
-
 $HOME/local/bin/python3 core8/pwb.py td_core/mdcount/words newpages
 
