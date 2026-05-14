d9ce5bac455bfd2962ca24c92f7973e318a1fe05
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-15 01:37:51 +0300

    .

---
+++ public_html/fix_duplicate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/data/project/mdwiki/local/bin:/usr/local/bin:/usr/bin:/bin
+
+toolforge jobs run fixduplict --image python3.9 --command "/data/project/mdwiki/local/bin/python3 core8/pwb.py mdpy/fix_duplicate save"
