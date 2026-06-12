9b9d327c235dfc2b13247af320dbf948240fa821
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-06-12 09:02:06 +0300

    Update import-history.php

--- public_html/import-history.php
+++ public_html/import-history.php
@@ -1,7 +1,9 @@
 <?php
-
+/*
 include_once __DIR__ . '/header.php';
 
 include __DIR__ . '/tools/import-history.php';
 
 include_once __DIR__ . '/footer.php';
+*/
+header("Location: https://mdw.toolforge.org/public_jobs/import_history");
