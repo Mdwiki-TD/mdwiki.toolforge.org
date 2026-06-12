e6d1b22e4d4c5fccadad826031b3042e6161c656
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-06-12 09:01:16 +0300

    Update redirect.php

--- public_html/redirect.php
+++ public_html/redirect.php
@@ -1,7 +1,9 @@
 <?php
-
+/*
 include_once __DIR__ . '/header.php';
 
 include __DIR__ . '/tools/redirect.php';
 
 include_once __DIR__ . '/footer.php';
+*/
+header("Location: https://mdw.toolforge.org/public_jobs/create_redirects");
