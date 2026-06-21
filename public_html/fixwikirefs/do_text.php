c007a65524999f830094b187685bdb35754b4142
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-21 03:34:18 +0300

    .

--- public_html/fixwikirefs/do_text.php
+++ public_html/fixwikirefs/do_text.php
@@ -1,5 +1,4 @@
 <?php
-
 header('Content-Type: application/json');
 
 include_once __DIR__ . '/include.php';
--- public_html/mdwiki4.php
+++ public_html/mdwiki4.php
@@ -1,10 +1,2 @@
 <?php
-/*
-include_once __DIR__ . '/header.php';
-
-include_once __DIR__ . '/tools/mdwiki4/index.php';
-
-include_once __DIR__ . '/footer.php';
-*/
-
 header("Location: https://mdw.toolforge.org/newupdater/");
