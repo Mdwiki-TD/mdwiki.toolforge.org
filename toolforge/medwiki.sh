ea861509790281fde5b3e53948e53019a28146f3
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-22 05:05:44 +0300

    .

--- toolforge/medwiki.sh
+++ toolforge/medwiki.sh
@@ -2,7 +2,6 @@
 
 
 cp -rn /data/project/mdwiki/public_html/Translation_Dashboard /data/project/medwiki/public_html -v
-cp -rn /data/project/mdwiki/confs /data/project/medwiki -v
 cp -rn /data/project/mdwiki/shs /data/project/medwiki -v
 cp -rn /data/project/mdwiki/*.yaml /data/project/medwiki -v
 cp -rn /data/project/mdwiki/user-config.py /data/project/medwiki -v
